5dabfd03bb1eb4a947cdb85f6052f4c9035adfa0
Ahmed <0nani.ahmed@gmail.com>
2022-06-08 15:20:57 +0100

    fixed delete button

--- php/crudoperation.php
+++ php/crudoperation.php
@@ -17,30 +17,20 @@
                 $nums=$_POST['text1'];
                 $noms=$_POST['text2'];
                 $stat->execute();
-                if(isset($_POST['text1'])){
-                    $stat->execute();
-                }
-                else{
-                    echo "Num du service required";
-                }
-
-                 // -*-*-*-*-*-*-**-*- Delete row in table Service -*-*-*-*-*-*-**-*-
-
-                 if(isset($_POST["bDelete"]))
-                 {
-                     $connection = new PDO("mysql:host=$servername;dbname=$db",$user,$pass);;
-                     $query="DELETE FROM service WHERE nums=:a , noms=:b";
-                     $stat=$connection->prepare($query);
-                     $stat->bindParam(":a",$_POST["nums"]);
-                     $stat->bindParam(":b",$_POST["noms"]);
-                     $nums=$_POST['text1'];
-                     $noms=$_POST['text2'];
-                     $stat->execute();
-                    echo "service deleted";
-
+            }
+            if(isset($_POST['bDelete'])){
+                {
+                $connection = new PDO("mysql:host=$servername;dbname=$db",$user,$pass);
+                $query ="DELETE FROM service WHERE nums=:a";
+                $stat=$connection->prepare($query);
+                $stat->bindParam(":a",$_POST["text1"]);
+                $stat->execute();
+                echo "service deleted successfully";
             }
         }
 
+                 // -*-*-*-*-*-*-**-*- Delete row in table Service -*-*-*-*-*-*-**-*-
+                
     }
         catch(PDOException $e){
             echo "Error" . $e->getMessage();
